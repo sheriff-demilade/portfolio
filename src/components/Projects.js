db41ed800516a477e755740adbe3f4c9128a54a1
sheriff-demilade <sheriffdemilade@gmail.com>
2023-12-05 11:01:48 +0100

    updated some text

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -63,13 +63,7 @@ export const Projects = () => {
                   }
                 >
                   <h2>Projects</h2>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book.
-                  </p>
+                  <p>These are the projects I have built using React Js.</p>
                   <Tab.Container id="projects-tabs" defaultActiveKey="first">
                     <Nav
                       variant="pills"
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -35,11 +35,7 @@ export const Skills = () => {
           <div className="col-12">
             <div className="skill-bx wow zoomIn">
               <h2>Skills</h2>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry.<br></br> Lorem Ipsum has been the industry's standard
-                dummy text.
-              </p>
+              <p>These are my Techinical skills.</p>
               <Carousel
                 responsive={responsive}
                 infinite={true}
